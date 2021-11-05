452c0412a053746d000995e343a20ef90bcb1df3
Jamoliddin Saidov <sjamoliddin21@gmail.com>
2021-11-05 17:36:04 +0500

    add dashboard product content

---
+++ src/assets/contents/Dashboard/ProjectsCardContent.js
@@ -0,0 +1,65 @@
+// logos
+import img1 from '../../images/small-logos/logo-xd.svg'
+import img2 from '../../images/small-logos/logo-atlassian.svg'
+import img3 from '../../images/small-logos/logo-slack.svg'
+import img4 from '../../images/small-logos/logo-spotify.svg'
+import img5 from '../../images/small-logos/logo-jira.svg'
+import img6 from '../../images/small-logos/logo-invision.svg'
+
+// people
+import person1 from '../../images/ivana-square.jpg'
+import person2 from '../../images/ivana-squares.jpg'
+import person3 from '../../images/team-4.jpg'
+import person4 from '../../images/team-1.jpg'
+import person5 from '../../images/team-2.jpg'
+import person6 from '../../images/kal-visuals-square.jpg'
+import person7 from '../../images/marie.jpg'
+import person8 from '../../images/team-3.jpg'
+import person9 from '../../images/team-5.jpg'
+
+export const ProjectsCardTableHeaders = ['Companies', 'Members', 'Budget', 'Completion']
+
+export const ProjectsCardTableBody = [
+  {
+    companyImg: img1,
+    companyName: 'Soft UI XD Version',
+    members: [person1, person2, person3, person4],
+    budget: '$14,000',
+    completition: 60,
+  },
+  {
+    companyImg: img2,
+    companyName: 'Add Progress Track',
+    members: [person5, person6],
+    budget: '$3,000',
+    completition: 10,
+  },
+  {
+    companyImg: img3,
+    companyName: 'Fix Platform Errors',
+    members: [person7, person8],
+    budget: 'Not set',
+    completition: 100,
+  },
+  {
+    companyImg: img4,
+    companyName: 'Launch our Mobile App',
+    members: [person9, person1, person2, person3],
+    budget: '$20,500',
+    completition: 100,
+  },
+  {
+    companyImg: img5,
+    companyName: 'Add the New Pricing Page',
+    members: [person4],
+    budget: '$500',
+    completition: 25,
+  },
+  {
+    companyImg: img6,
+    companyName: 'Redesign New Online Shop',
+    members: [person5, person6],
+    budget: '$2000',
+    completition: 40,
+  },
+]
