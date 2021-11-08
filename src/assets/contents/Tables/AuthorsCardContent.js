c71dc5fd6e5ad2cb7b748e59ea1ca09aa09c92c2
Jamoliddin Saidov <sjamoliddin21@gmail.com>
2021-11-08 13:40:37 +0500

    add authors content

---
+++ src/assets/contents/Tables/AuthorsCardContent.js
@@ -0,0 +1,70 @@
+// people
+import person1 from '../../images/bruce-mars.jpg'
+import person2 from '../../images/team-3.jpg'
+import person3 from '../../images/team-2.jpg'
+import person4 from '../../images/team-4.jpg'
+import person5 from '../../images/team-1.jpg'
+import person6 from '../../images/marie.jpg'
+
+export const AuthorsCardContent = [
+  {
+    img: person1,
+    name: 'John Michael',
+    email: 'john@company.com',
+    jobTitle: 'Manager',
+    jobType: 'Organization',
+    status: 'online',
+    employedDate: '23/04/18',
+    action: 'edit',
+  },
+  {
+    img: person2,
+    name: 'Alexa Liras',
+    email: 'alexa@company.com',
+    jobTitle: 'Programator',
+    jobType: 'Developer',
+    status: 'offline',
+    employedDate: '11/01/19',
+    action: 'edit',
+  },
+  {
+    img: person3,
+    name: 'Laurent Perrier',
+    email: 'laurent@company.com',
+    jobTitle: 'Executive',
+    jobType: 'Projects',
+    status: 'online',
+    employedDate: '19/09/17',
+    action: 'edit',
+  },
+  {
+    img: person4,
+    name: 'Michael Levi',
+    email: 'michael@company.com',
+    jobTitle: 'Programator',
+    jobType: 'Developer',
+    status: 'online',
+    employedDate: '24/12/08',
+    action: 'edit',
+  },
+  {
+    img: person5,
+    name: 'Richard Gran',
+    email: 'richard@company.com',
+    jobTitle: 'Manager',
+    jobType: 'Executive',
+    status: 'offline',
+    employedDate: '04/10/21',
+    action: 'edit',
+  },
+  {
+    img: person6,
+    name: 'Marie Watson',
+    email: 'Marie@company.com',
+    jobTitle: 'Programtor',
+    jobType: 'Developer',
+    status: 'offline',
+    employedDate: '14/09/20',
+    action: 'edit',
+  },
+]
