cd04620cb7f1857fd2d640ad64244914f83872d0
Jamoliddin Saidov <sjamoliddin21@gmail.com>
2021-11-08 16:46:58 +0500

    add tables project content

---
+++ src/assets/contents/Tables/ProjectsCardContent.js
@@ -0,0 +1,60 @@
+import img1 from '../../images/small-logos/logo-xd.svg'
+import img2 from '../../images/small-logos/logo-atlassian.svg'
+import img3 from '../../images/small-logos/logo-slack.svg'
+import img4 from '../../images/small-logos/logo-spotify.svg'
+import img5 from '../../images/small-logos/logo-jira.svg'
+import img6 from '../../images/small-logos/logo-invision.svg'
+import { GoKebabVertical } from 'react-icons/go'
+
+export const ProjectsCardTableHeaders = ['Companies', 'Members', 'Budget', 'Completion']
+
+export const ProjectsCardTableBody = [
+  {
+    img: img4,
+    name: 'Spotift',
+    budget: '$2,500',
+    status: 'working',
+    completition: 60,
+    action: <GoKebabVertical />,
+  },
+  {
+    img: img6,
+    name: 'Invesion',
+    budget: '$5,000',
+    status: 'done',
+    completition: 100,
+    action: <GoKebabVertical />,
+  },
+  {
+    img: img5,
+    name: 'Invesion',
+    budget: '$3,400',
+    status: 'canceled',
+    completition: 30,
+    action: <GoKebabVertical />,
+  },
+  {
+    img: img3,
+    name: 'Slack',
+    budget: '$1,400',
+    status: 'canceled',
+    completition: 0,
+    action: <GoKebabVertical />,
+  },
+  {
+    img: img2,
+    name: 'Atlassian',
+    budget: '$1,4000',
+    status: 'working',
+    completition: 80,
+    action: <GoKebabVertical />,
+  },
+  {
+    img: img1,
+    name: 'Adobe XD',
+    budget: '$2,300',
+    status: 'done',
+    completition: 100,
+    action: <GoKebabVertical />,
+  },
+]
