9b301881f468eb4e07075f9e5ad19f8ce8be903c
Jamoliddin Saidov <sjamoliddin21@gmail.com>
2021-11-09 15:39:44 +0500

    add budget card content

---
+++ src/assets/contents/Billings/BudgetCardContent.js
@@ -0,0 +1,17 @@
+import { MdAccountBalance } from 'react-icons/md'
+import { FaPaypal } from 'react-icons/fa'
+
+export const BudgetCardContent = [
+  {
+    icon: <MdAccountBalance />,
+    title: 'Bank',
+    text: 'Belong Interactive',
+    budget: '+$2000',
+  },
+  {
+    icon: <FaPaypal />,
+    title: 'Paypal',
+    text: 'Freelance Payment',
+    budget: '$455.00',
+  },
+]
