549f806e2f550cd260bddacfbf2ed065c2b6bc33
Jamoliddin Saidov <sjamoliddin21@gmail.com>
2021-11-09 16:12:47 +0500

    add payment card content

---
+++ src/assets/contents/Billings/PaymentCardContent.js
@@ -0,0 +1,16 @@
+import mastercard from '../../images/logos/mastercard.png'
+import visa from '../../images/logos/visa.png'
+import { MdMode } from 'react-icons/md'
+
+export const PaymentCardContent = [
+  {
+    img: mastercard,
+    cardNumber: '****  ****  ****  7852',
+    icon: <MdMode />,
+  },
+  {
+    img: visa,
+    cardNumber: '****  ****  ****  5248',
+    icon: <MdMode />,
+  },
+]
