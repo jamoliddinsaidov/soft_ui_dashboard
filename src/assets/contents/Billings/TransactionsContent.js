000828657713a592f639228a7cdafa4062617c3a
Jamoliddin Saidov <sjamoliddin21@gmail.com>
2021-11-10 14:07:24 +0500

    add transactions content

---
+++ src/assets/contents/Billings/TransactionsContent.js
@@ -0,0 +1,43 @@
+import { MdArrowCircleUp, MdArrowCircleDown, MdOutlinePending } from 'react-icons/md'
+
+export const TransactionsNew = [
+  {
+    icon: <MdArrowCircleDown />,
+    title: 'Netflix',
+    trDate: '27 March 2020, at 12:30 PM',
+    budget: '- $ 2,500',
+  },
+  {
+    icon: <MdArrowCircleUp />,
+    title: 'Apple',
+    trDate: '27 March 2020, at 04:30 AM',
+    budget: '+ $ 2,000',
+  },
+]
+
+export const TransactionsOld = [
+  {
+    icon: <MdArrowCircleUp />,
+    title: 'Stripe',
+    trDate: '26 March 2020, at 13:45 PM',
+    budget: '+ $ 7,50',
+  },
+  {
+    icon: <MdArrowCircleUp />,
+    title: 'HubSpot',
+    trDate: '26 March 2020, at 12:30 PM',
+    budget: '+ $ 1,000',
+  },
+  {
+    icon: <MdArrowCircleUp />,
+    title: 'Creative Tim',
+    trDate: '26 March 2020, at 12:30 PM',
+    budget: '+ $ 1,000',
+  },
+  {
+    icon: <MdOutlinePending />,
+    title: 'Webflow',
+    trDate: '26 March 2020, at 05:00 AM',
+    budget: 'Pending',
+  },
+]
