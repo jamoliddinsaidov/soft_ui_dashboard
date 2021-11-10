ed4898a9e33838ebcf3f060f1fac1a37679a8df0
Jamoliddin Saidov <sjamoliddin21@gmail.com>
2021-11-10 11:55:20 +0500

    add billing info content

---
+++ src/assets/contents/Billings/BillingInformationContent.js
@@ -0,0 +1,28 @@
+import { MdDelete, MdMode } from 'react-icons/md'
+
+export const BillingInformationContent = [
+  {
+    name: 'Oliver Liam',
+    iconDelete: <MdDelete />,
+    iconEdit: <MdMode />,
+    companyName: 'Viking Burrito',
+    emailAddress: 'oliver@burrito.com',
+    vatNum: 'FRB1235476',
+  },
+  {
+    name: 'Lucas Harper',
+    iconDelete: <MdDelete />,
+    iconEdit: <MdMode />,
+    companyName: 'Stone Tech Zone',
+    emailAddress: 'lucas@stone-tech.com',
+    vatNum: 'FRB1235476',
+  },
+  {
+    name: 'Ethan James',
+    iconDelete: <MdDelete />,
+    iconEdit: <MdMode />,
+    companyName: 'Fiber Notion',
+    emailAddress: 'ethan@fiber.com',
+    vatNum: 'FRB1235476',
+  },
+]
