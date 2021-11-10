a42871050e2249e38da1417da5e92d81ffe83c74
Jamoliddin Saidov <sjamoliddin21@gmail.com>
2021-11-10 10:59:18 +0500

    add invoices content

---
+++ src/assets/contents/Billings/InvoiceContent.js
@@ -0,0 +1,54 @@
+import { MdPictureAsPdf } from 'react-icons/md'
+
+export const InvoiceContent = [
+  {
+    date: 'February, 10, 2021',
+    id: '#RV-126749',
+    budget: '$250',
+    action: (
+      <div>
+        <MdPictureAsPdf /> PDF
+      </div>
+    ),
+  },
+  {
+    date: 'April, 05, 2020',
+    id: '#QW-103578',
+    budget: '$120',
+    action: (
+      <div>
+        <MdPictureAsPdf /> PDF
+      </div>
+    ),
+  },
+  {
+    date: 'March, 01, 2020',
+    id: '#MS-415646',
+    budget: '$180',
+    action: (
+      <div>
+        <MdPictureAsPdf /> PDF
+      </div>
+    ),
+  },
+  {
+    date: 'June, 25, 2019',
+    id: '#MS-415646',
+    budget: '$180',
+    action: (
+      <div>
+        <MdPictureAsPdf /> PDF
+      </div>
+    ),
+  },
+  {
+    date: 'March, 01, 2019',
+    id: '#AR-803481',
+    budget: '$300',
+    action: (
+      <div>
+        <MdPictureAsPdf /> PDF
+      </div>
+    ),
+  },
+]
