ff1ff1d9ceabe956e1536a37412f9a52883f67b9
Jamoliddin Saidov <sjamoliddin21@gmail.com>
2021-12-27 11:46:40 +0500

    add profile header

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -3,14 +3,33 @@ import React from 'react'
 // components
 import MainLayout from '../../layouts/MainLayout'
 import ProfileTopHeader from '../../components/Headers/ProfileTopHeader'
+import MainProfileInfo from '../../components/ImageAndTexts/ImageAndTexts'
+import IconAndText from '../../components/ImageAndTexts/IconAndText'
 
 // styles
-import { StyleProfileTopHeader } from './styles/StyledProfile'
+import { StypedProfileHeader, ProfileTopHeaderIcons } from './styles/StyledProfile'
+
+// contents
+import { ProfileInfoContent, ProfileTopHeaderIconContent } from '../../assets/contents/Profile/ProfileInfoContent'
 
 const Profile = () => {
   return (
     <MainLayout>
       <ProfileTopHeader page='Profile' />
+      <StypedProfileHeader>
+        <MainProfileInfo
+          image={ProfileInfoContent.img}
+          title={ProfileInfoContent.name}
+          subtitle={ProfileInfoContent.msg}
+          borderRadius={20}
+          imgWidth={13}
+        />
+        <ProfileTopHeaderIcons>
+          {ProfileTopHeaderIconContent.map((content, index) => (
+            <IconAndText icon={content.icon} text={content.title} key={index + content.title} />
+          ))}
+        </ProfileTopHeaderIcons>
+      </StypedProfileHeader>
       <h2>Profile page content goes here</h2>
     </MainLayout>
   )
--- src/pages/Profile/styles/StyledProfile.js
+++ src/pages/Profile/styles/StyledProfile.js
@@ -2,6 +2,11 @@ import styled from 'styled-components'
 import { Flex, FlexBetween, colors } from '../../../GlobalStyles'
 import { StyledTopHeader } from '../../../components/Headers/styles/TopHeader'
 
-export const StyleProfileTopHeader = styled(StyledTopHeader)`
-  background: red;
+export const StypedProfileHeader = styled(StyledTopHeader)`
+  width: 95%;
+  margin: -3em auto 2em;
+  padding: 1.5em 1em;
+  box-shadow: rgb(255 255 255 / 90%) 0rem 0rem 0.0625rem 0.0625rem inset, rgb(0 0 0 / 5%) 0rem 1.25rem 1.6875rem 0rem;
 `
+
+export const ProfileTopHeaderIcons = styled(FlexBetween)``
