523cf6e3036e2f50fc703495c6e1e81f0bac0613
Jamoliddin Saidov <sjamoliddin21@gmail.com>
2021-12-29 17:22:54 +0500

    add profile projects content

---
+++ src/assets/contents/Profile/ProfileProjectsContent.js
@@ -0,0 +1,31 @@
+import project1 from '../../images/home-decor-1/jpg'
+import project2 from '../../images/home-decor-2/jpg'
+import project3 from '../../images/home-decor-3/jpg'
+import person1 from '../../images/bruce-mars.jpg'
+import person2 from '../../images/team-3.jpg'
+import person3 from '../../images/team-2.jpg'
+import person4 from '../../images/team-4.jpg'
+
+export const ProfileProjectsContent = [
+  {
+    img: project1,
+    number: 'Project #1',
+    name: 'Modern',
+    description: 'As Uber works through a huge amount of internal management turmoil.',
+    people: [person1, person2, person3, person4],
+  },
+  {
+    img: project2,
+    number: 'Project #2',
+    name: 'Scandinavian',
+    description: 'Music is something that every person has his or her own specific opinion about.',
+    people: [person1, person2, person3, person4],
+  },
+  {
+    img: project3,
+    number: 'Project #3',
+    name: 'Minimalist',
+    description: 'Different people have different taste, and various types of music.',
+    people: [person1, person2, person3, person4],
+  },
+]
